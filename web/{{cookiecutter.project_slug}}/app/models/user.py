243cb7f0d7f25c14864c520cde1a8af4ef1cf4ef
codingpy <1658046076@qq.com>
2023-06-26 23:36:58 +0800

    feat: add user model

---
+++ web/{{cookiecutter.project_slug}}/app/models/user.py
@@ -0,0 +1,12 @@
+from sqlalchemy.orm import Mapped, mapped_column
+
+from .base import Base
+
+
+class User(Base):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    email: Mapped[str] = mapped_column(unique=True)
+    hashed_password: Mapped[str]
+    full_name: Mapped[str] = mapped_column(index=True)
+    is_active: Mapped[bool] = mapped_column(default=True)
+    is_superuser: Mapped[bool] = mapped_column(default=False)
