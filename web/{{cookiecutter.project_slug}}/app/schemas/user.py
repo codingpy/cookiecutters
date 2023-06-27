424ab6730420d66e773ce36019d4e8740cb04779
codingpy <1658046076@qq.com>
2023-06-28 00:22:39 +0800

    feat: add user schemas

---
+++ web/{{cookiecutter.project_slug}}/app/schemas/user.py
@@ -0,0 +1,34 @@
+from typing import Union
+
+from pydantic import BaseModel, EmailStr
+
+
+class UserBase(BaseModel):
+    email: Union[EmailStr, None] = None
+    full_name: Union[str, None] = None
+    is_active: bool = True
+    is_superuser: bool = False
+
+
+class UserCreate(UserBase):
+    email: EmailStr
+    password: str
+
+
+class UserUpdate(UserBase):
+    password: Union[str, None] = None
+
+
+class UserInDBBase(UserBase):
+    id: int
+
+    class Config:
+        orm_mode = True
+
+
+class User(UserInDBBase):
+    pass
+
+
+class UserInDB(UserInDBBase):
+    hashed_password: str
