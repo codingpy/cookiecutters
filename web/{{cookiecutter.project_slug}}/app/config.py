95dfeb31f007722d36dfbc825114232f8c82d298
codingpy <1658046076@qq.com>
2023-07-02 18:41:57 +0800

    refactor: rename settings field

--- web/{{cookiecutter.project_slug}}/app/config.py
+++ web/{{cookiecutter.project_slug}}/app/config.py
@@ -1,5 +1,5 @@
 import secrets
-from typing import Union
+from typing import Any, Union
 
 from pydantic import BaseSettings, PostgresDsn, validator
 
@@ -9,23 +9,23 @@ class Settings(BaseSettings):
     secret_key: str = secrets.token_urlsafe(32)
     access_token_expire_minutes: int = 8 * 24 * 60
 
-    postgres_host: str
+    postgres_server: str
     postgres_user: str
     postgres_password: str
     postgres_db: str
     sqlalchemy_database_uri: Union[PostgresDsn, None] = None
 
     @validator("sqlalchemy_database_uri", pre=True)
-    def assemble_db_connection(cls, v: Union[str, None], values: dict[str, str]) -> str:
+    def assemble_db_connection(cls, v: Union[str, None], values: dict[str, Any]) -> str:
         if isinstance(v, str):
             return v
 
         return PostgresDsn.build(
             scheme="postgresql+asyncpg",
-            user=values.get("postgres_user"),
-            password=values.get("postgres_password"),
-            host=values.get("postgres_host"),
-            path=f"/{values.get('postgres_db')}",
+            user=values["postgres_user"],
+            password=values["postgres_password"],
+            host=values["postgres_server"],
+            path=f"/{values['postgres_db']}",
         )
 
     class Config:
