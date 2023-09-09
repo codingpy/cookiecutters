9b51509443af2f705af782160ec64944a720f25c
codingpy <1658046076@qq.com>
2023-09-09 17:47:30 +0800

    feat: init db

--- web/{{cookiecutter.project_slug}}/backend/app/app/db.py
+++ web/{{cookiecutter.project_slug}}/backend/app/app/db.py
@@ -1,6 +1,24 @@
-from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine
+from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
 
+from app import crud, schemas
 from app.config import settings
 
 engine = create_async_engine(settings.sqlalchemy_database_uri)
 async_session = async_sessionmaker(engine, expire_on_commit=False)
+
+
+async def init_db(db: AsyncSession) -> None:
+    # Tables should be created with Alembic migrations
+    # But if you don't want to use migrations, create
+    # the tables un-commenting the next line
+    # Base.metadata.create_all(bind=engine)
+
+    if await crud.user.exists_email(db, settings.first_superuser):
+        return
+
+    user_in = schemas.UserCreate(
+        email=settings.first_superuser,
+        password=settings.first_superuser_password,
+        is_superuser=True,
+    )
+    await crud.user.create(db, user_in)
--- web/{{cookiecutter.project_slug}}/backend/app/tests/api/api_v1/test_users.py
+++ web/{{cookiecutter.project_slug}}/backend/app/tests/api/api_v1/test_users.py
@@ -1,18 +1,14 @@
-from typing import TYPE_CHECKING
-
 import pytest
+from httpx import AsyncClient
 
 from app.config import settings
 
-if TYPE_CHECKING:
-    from httpx import AsyncClient
-
-
+# This is the same as using the @pytest.mark.anyio on all test functions in the module
 pytestmark = pytest.mark.anyio
 
 
 async def test_get_users_superuser_me(
-    client: "AsyncClient", superuser_token_headers: dict[str, str]
+    client: AsyncClient, superuser_token_headers: dict[str, str]
 ) -> None:
     r = await client.get(
         f"{settings.api_v1_str}/users/me", headers=superuser_token_headers
